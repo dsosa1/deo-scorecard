890177a863c3127276a361279f25ac3510367de6
Daniel Sosa <dsosa045@gmail.com>
2026-04-15 14:46:46 -0400

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,6 +1,12 @@
-import { sql } from '@vercel/postgres';
+import { neon } from '@neondatabase/serverless';
+
+export function getDb() {
+  const sql = neon(process.env.DATABASE_URL);
+  return sql;
+}
 
 export async function initDb() {
+  const sql = getDb();
   await sql`
     CREATE TABLE IF NOT EXISTS entries (
       id SERIAL PRIMARY KEY,
